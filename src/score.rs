735065017a722f21526f10d31b42b10a6d8f7d4f
Hadrien Renaud <hadrien.renaud.22@ucl.ac.uk>
2022-10-01 19:23:52 +0100

    [score] Display score

--- src/score.rs
+++ src/score.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
 
+use crate::GameState;
+use crate::loading::{FontAssets, TextureAssets};
+
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Score>();
+        app.init_resource::<Score>()
+            .add_system_set(SystemSet::on_enter(GameState::Cooking).with_system(init_score))
+            .add_system_set(SystemSet::on_update(GameState::Cooking).with_system(update_score));
     }
 }
 
@@ -20,21 +25,56 @@ impl Default for Score {
         Self {
             score: 0,
             streak: 0,
-            lives: 3
+            lives: 3,
         }
     }
 }
 
 impl Score {
-    pub fn compute_on_failure (&mut self) {
+    pub fn compute_on_failure(&mut self) {
         self.streak = 0;
-        self.lives -=1;
+        self.lives -= 1;
     }
 
     pub fn compute_on_success(&mut self, time: f64, difficulty: usize) {
         self.streak += 1;
         // TODO pb : streak should not inflate your score this time if it negative
         self.score += self.streak * (5 * (difficulty as i64 - 1) - (time.round() as i64));
-        println!("Updated score: {} (with current streak: {})", self.score, self.streak);
+    }
+
+    fn to_display_text(&self) -> String {
+        format!("Score {}", self.score)
+    }
+}
+
+#[derive(Component)]
+struct ScoreUI;
+
+fn init_score(score: Res<Score>, mut commands: Commands, fonts: Res<FontAssets>) {
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: score.to_display_text(),
+                    style: TextStyle {
+                        font: fonts.axones_gold.clone(),
+                        font_size: 16.0,
+                        color: Color::WHITE,
+                    },
+                }],
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(256., 16., 1.),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(ScoreUI);
+}
+
+fn update_score(score: Res<Score>, mut query: Query<&mut Text, With<ScoreUI>>) {
+    for mut text in &mut query {
+        text.sections[0].value = score.to_display_text();
     }
 }
